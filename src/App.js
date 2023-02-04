e5c1f1fcf552dd586733d4d282ac9e237e3c5961
sho-itsuka <sho.itsuka122@gmail.com>
2023-02-04 20:58:36 +0900

    defined loading

--- src/App.js
+++ src/App.js
@@ -1,27 +1,32 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import './App.css';
 
 import { getAllPokemon } from './utils/pokemon';
 
 function App() {
   const initialURL = "https://pokeapi.co/api/v2/pokemon";
-  // リロード時にポケモンのデータを全て取得
-  // 呼び出しは一度だけなので、第二引数は空の配列で指定
+  const [loading, setLoading] = useState(true);
+
   useEffect(() => {
     const fetchPokemonData = async () => {
       // 全てのポケモンデータを取得
-      // awaitで、getAllPokemon(Promiseの処理)が終わるまで待たせる処理
       let res = await getAllPokemon(initialURL);
       console.log(res)
+      setLoading(false);
     };
     fetchPokemonData();
   }, [])
 
 
-  return (
-    <div className="App">
-    </div>
-  );
+  return <div className="App">
+    {loading ? (
+      <h1>ロード中・・・</h1>
+    ) : (
+      <>
+        <h1>ポケモンデータを取得しました</h1>
+      </>
+    )}
+  </div>;
 }
 
 export default App;
