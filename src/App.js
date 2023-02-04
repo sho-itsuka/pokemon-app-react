66d05b4ca618e89ecb4e26b72f33e4db7b9a4512
sho-itsuka <sho.itsuka122@gmail.com>
2023-02-04 18:33:30 +0900

    全てのポケモンデータを取得

--- src/App.js
+++ src/App.js
@@ -1,6 +1,18 @@
+import { useEffect } from 'react';
 import './App.css';
 
 function App() {
+  const initialURL = "https://pokeapi.co/api/v2/pokemon";
+  // リロード時にポケモンのデータを全て取得
+  // 呼び出しは一度だけなので、第二引数は空の配列で指定
+  useEffect(() => {
+    const fetchPokemonData = async () => {
+      // 全てのポケモンデータを取得
+      let res = await getAllPokemon();
+    };
+  }, [])
+
+
   return (
     <div className="App">
     </div>
