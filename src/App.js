0e4a6431026bf02aeade9c7fad085d0ac9f5bc0c
sho-itsuka <sho.itsuka122@gmail.com>
2023-02-04 22:07:46 +0900

    詳細データの取得

--- src/App.js
+++ src/App.js
@@ -6,7 +6,9 @@ import { getPokemon }    from './utils/pokemon';
 
 function App() {
   const initialURL = "https://pokeapi.co/api/v2/pokemon";
-  const [loading, setLoading] = useState(true);
+
+  const [loading, setLoading]        = useState(true);
+  const [pokemonData, setPokemonData] = useState();
 
   useEffect(() => {
     const fetchPokemonData = async () => {
@@ -14,21 +16,22 @@ function App() {
       let res = await getAllPokemon(initialURL);
       // 各ポケモンの詳細なデータを取得
       loadPokemon(res.results);
-      console.log(res.results)
       setLoading(false);
     };
     fetchPokemonData();
   }, [])
 
-  const loadPokemon = (data) => {
+  const loadPokemon = async (data) => {
     // _pokemonDataの中にポケモン1つ1つのデータを格納する
-    let _pokemonData = Promise.all(
+    let _pokemonData = await Promise.all(
       data.map((pokemon) => {
         let pokemonRecord = getPokemon(pokemon.url);
         return pokemonRecord;
       })
     );
+    setPokemonData(_pokemonData);
   };
+
 
   return <div className="App">
     {loading ? (
